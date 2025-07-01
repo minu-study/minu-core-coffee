3c22f4c279ce07e9f7d02abe10b4b02e2a5cd5c4
minu <min9288@gmail.com>
2025-07-01 22:45:42 +0900

    fix: 빈 충돌로 인한 클래스명 수정

--- src/main/java/minu/coffee/common/filter/customException/GlobalCustomExceptionHandler.java
+++ src/main/java/minu/coffee/common/filter/customException/GlobalCustomExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalCustomExceptionHandler {
 
     @ExceptionHandler(AppException.class)
     public ResponseEntity<ApiResponse> handleAppException(AppException ex) {
